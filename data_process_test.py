3a5e275a45e4a2507b321c510510166677daff09
scy0416 <scy0416@gmail.com>
2025-10-23 21:11:58 +0900

    불필요한 파일 삭제

--- data_process_test.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df1 = pd.read_csv("dataset/big_data_set1_f.csv", encoding="cp949")
-df2 = pd.read_csv("dataset/big_data_set2_f.csv", encoding="cp949")
-df3 = pd.read_csv("dataset/big_data_set3_f.csv", encoding="cp949")

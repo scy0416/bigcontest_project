e34e2fdee4e46e25c8bbcc6db8e3915eae9d6ba9
scy0416 <scy0416@gmail.com>
2025-10-23 21:43:27 +0900

    가맹점 선택 코드 정리

--- brand_recognize.py
+++ brand_recognize.py
@@ -2,20 +2,25 @@ import streamlit as st
 import pandas as pd
 from components import *
 
+# 브랜드와 업계 졍보 초기화
 st.session_state['brand'] = None
 st.session_state['works'] = None
+
+# 가맹점 정보 로드
 @st.cache_resource
 def load_brand_data():
     df = pd.read_csv("dataset/big_data_set1_f.csv", encoding="cp949")
 
     tmp_list = []
 
+    # 지역, 이름, 업종, 식별코드, 개설일만을 추출
     for idx, row in df.iterrows():
         tmp = {'지역': row['MCT_SIGUNGU_NM'], '이름': row['MCT_NM'], '업종': row['HPSN_MCT_ZCD_NM'], '식별코드': row['ENCODED_MCT'], '개설일': row['ARE_D']}
         tmp_list.append(tmp)
 
     return tmp_list
 
+# 업종 정보만 추출
 @st.cache_resource
 def load_works():
     works = set()
@@ -24,6 +29,7 @@ def load_works():
         works.add(row['HPSN_MCT_ZCD_NM'])
     return list(works)
 
+# 이름 문자열을 처리하는 메소드
 def process_name(name):
     if name == "":
         return ""
@@ -33,47 +39,57 @@ def process_name(name):
         return f"{name[0]}*"
     return f"{name[:2]}{'*'*len(name[2:])}"
 
+# 브랜드 정보 설정 처리
 def set_brand_info(brand):
     st.session_state['selected_brand'] = brand
 
+# 사이드바 초기화
 init_sidebar()
 
+# 브랜드 정보들 로드
 with st.spinner("브랜드 정보 로드 중..."):
     st.session_state['brand'] = load_brand_data()
     st.session_state['works'] = load_works()
 
-#st.write(st.session_state['brand'])
 st.set_page_config(layout="wide")
 
 select, selected = st.columns(2, border=True)
 
+# 브랜드 검색 칼럼
 with select:
     col1, col2 = st.columns(2)
 
+    # 가맹점 검색
     with col1:
         st.subheader("가맹점명")
         st.text_input(label="a", label_visibility="collapsed", key="name")
 
+    # 업종 검색
     with col2:
         st.subheader("업종")
-        #st.text_input(label="a", label_visibility="collapsed", key="work")
         st.selectbox(label='a', label_visibility="collapsed", key="work", options=st.session_state['works'])
 
+    # 가맹점과 업종을 통해서 가맹점 검색
     if st.button("검색", use_container_width=True):
         name = st.session_state["name"]
+        # 원래 이름
         original_name = name
+        # 처리된 이름
         processed_name = process_name(name)
         work = st.session_state["work"]
 
         st.subheader("검색 결과")
         search_result = []
         for brand in st.session_state["brand"]:
+            # 원래 이름이나 처리된 이름이 포함되지 않는 경우
             if not (original_name == "" or (original_name in brand['이름'] or processed_name in brand['이름'])):
                 continue
+            # 업종이 틀린 경우
             if brand['업종'] != work:
                 continue
+            # 가맹점 추가
             search_result.append(brand)
-        #st.write(search_result)
+
         with st.container(border=True):
             for brand in search_result:
                 # 이름, 지역, 업종, 개설일
@@ -90,12 +106,12 @@ with select:
                     st.button("선택", key=f"select_{brand['식별코드']}", on_click=set_brand_info, args=[brand])
                 st.divider()
 
+# 선택한 가맹점 정보 출력되는 칼럶
 with selected:
     st.header("선택된 가맹점 정보")
     if 'selected_brand' not in st.session_state:
         st.info("아직 가맹점이 선택되지 않았습니다")
     else:
-        #st.write(st.session_state['selected_brand'])
         with st.container(border=True):
             st.subheader("가맹점명")
             st.write(st.session_state['selected_brand']['이름'])
